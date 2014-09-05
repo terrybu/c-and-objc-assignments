1d156776e4ab27f3e6ff14f889fac8187cf0918d
Terry Bu <ktbucifer@gmail.com>
2014-09-05 17:25:54 -0400

    Finish reading file and making new objects exercise 23

--- #23/#23/main.m
+++ #23/#23/main.m
@@ -18,9 +18,8 @@ int main(int argc, const char * argv[])
         person1.name = @"Terry Bu";
         person1.address = @"2111 21st Rd, Astoria NY 11105";
         
-        NSLog(@"Hello, %@!", person1.name);
-        NSLog(@"%@", person1.address);
-        
+//        NSLog(@"Hello, %@!", person1.name);
+//        NSLog(@"%@", person1.address);
         
         //read from a file
         //*I originally had some path below involving NSBundle Mainbundle but it didn't work. Once simplified down to string form like below, it works
@@ -28,20 +27,36 @@ int main(int argc, const char * argv[])
         NSString* path = @"/Users/adityanarayan/Desktop/TerryBu/Input_Folder/names&addresses.txt";
         NSString* content = [NSString stringWithContentsOfFile:path encoding:NSASCIIStringEncoding error:NULL];
         
-        NSLog(@"%@", content);
-        
+//        NSLog(@"%@", content);
         //parse the content string delimited by new lines first
         //with the current text file, it will make 3 name-address pairs
         
-        //parse the content string delimited by comma next, make it into an array
-        //["Tiffany Lan", "Addresss"]
+        NSArray *lines = [content componentsSeparatedByString:@"\n"];
+        NSUInteger lines_count = [lines count];
+        NSLog(@"Jarvis says the # of lines we found in that file is %lu", (unsigned long)lines_count);
         
-        //The result we want is an array of objects
-        //In a loop, as soon as you have person.name = "Tiffany Lan" and person.address = "Something", push it into that result array
-        //At the end of all the loops, you should have an array of Person objects
-        //Custom class unless you specify is mutable
+        //when you initiate NSMutableArrays, remember to allocate it and then initialize it with nil like this. Otherwise it will throw errors when you try to addobjects to it
+        NSMutableArray *resultArray = [[NSMutableArray alloc] initWithObjects: nil];
         
+        for (int i = 0; i < lines_count; i++) {
+            NSString *line_target = lines[i];
+            NSArray *pair = [line_target componentsSeparatedByString:@", "];
+            //now in pair array, we have name at pair[0] and address pair[1]
+            //we then instantiate a person
+            Person *person = [[Person alloc]init];
+            person.name = pair[0];
+            person.address = pair[1];
+            //we now add the person to our resultArray
+            [resultArray addObject:person];
+        }
         
+        //final resultarray output test
+        NSLog(@"%@", [resultArray description]);
+
+        //just for testing purposes, loop through the resultarray, output all names and addresses
+        for (int i = 0; i < [resultArray count]; i++) {
+            NSLog(@"%@ lives at %@", [resultArray[i] name], [resultArray[i] address]);
+        }
         
     }
     return 0;
